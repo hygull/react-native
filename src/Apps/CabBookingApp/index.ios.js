6971953ddfa94feb92598fb72c1471a85313ddc7
admin <admin@MacBook-Pro.local>
2017-05-17 20:07:59 +0530

    5 created a simple app that displays Hello Message

--- src/Apps/CabBookingApp/index.ios.js
+++ src/Apps/CabBookingApp/index.ios.js
@@ -2,6 +2,11 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  * @flow
+ * {
+      "details" : {
+        "created_on" : "18 May 2017",
+        "aim" : "to create an application for mobile"
+      }
  */
 
 import React, { Component } from 'react';
@@ -13,16 +18,43 @@ import {
 } from 'react-native';
 
 export default class CabBookingApp extends Component {
+
+  constructor() {
+    super() //Calling the constructor of parent class
+    this.state = {
+          name : "Rishikesh Agrawani",
+          company : "Offlinetrend solutions pvt ltd",
+          languages : ["C", "C++", "Python", "Golang", "TypeScript", "JavaScript", "HTML", "CSS"],
+          // getLanguagesList : () => {
+          //       var s = "";
+          //       for(var i; i<this.languages.length; i++) {
+          //         if(i==this.languages.length-1){
+          //             s += this.languages[i] + "."
+          //         }else{
+          //             s += this.languages[i]+","
+          //         }
+          //       }
+          //       return s;
+          //     }
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Hello {this.state.name}
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
         </Text>
-        <Text style={styles.instructions}>
+        
+        <Text>
+          {this.state.getLanguagesList} {'\n'}
+          Comapany name : {this.state.company}
+        </Text>
+        <Text style={styles.instructions2}>
+          &lt;Instructions&gt;,{"\n"}
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
         </Text>
@@ -45,9 +77,15 @@ const styles = StyleSheet.create({
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: 'green',
     marginBottom: 5,
   },
+  instructions2:{
+    textAlign:"center",
+    color:"navy",
+    margin:5,
+  }
+
 });
 
 AppRegistry.registerComponent('CabBookingApp', () => CabBookingApp);
