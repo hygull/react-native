ce11c857045d34ae049896e0d76c454a82ca5b6f
hygull <rishikesh0014051992@gmail.com>
2017-05-22 18:17:23 +0530

    23 added Home Screen(Navigation)

--- src/Apps/CabBookingApp/App.js
+++ src/Apps/CabBookingApp/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import { AppRegistry, Text} from "react-native"
 import { StackNavigator } from "react-navigation"
 
+//Home Screen
 class CabBookingHomeScreen extends React.Component {
 	static navigationOptions = {
 		title: "Home",
@@ -10,7 +11,7 @@ class CabBookingHomeScreen extends React.Component {
 	render() {
 		return <Text style={
 							{color: "green", fontWeight: "bold", 
-							textAlign: "center", paddingTop: 20, fontFamily:"Arial"}
+							textAlign: "center", paddingTop: 20, fontFamily:"Verdana"}
 				}> 
 					Hello, programmers!
 				</Text>;
